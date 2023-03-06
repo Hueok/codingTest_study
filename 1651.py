e0794300b12454ade7e59e6e9411d83f10041224
Hueok <124287568+Hueok@users.noreply.github.com>
2023-03-06 19:58:59 +0900

    Basic code practice

---
+++ 1651.py
@@ -0,0 +1,35 @@
+import itertools, sys, collections
+
+N = int(sys.stdin.readline())
+
+S = []
+full = []
+result = []
+
+for _ in range(N):
+    S.append(sys.stdin.readline().rstrip())  #str
+
+for i in range(1,len(S)+1):
+    X = list(itertools.product(S, repeat=i))
+
+    for item in X:
+        full.append(''.join(item))
+
+counter = collections.Counter(full)
+
+val = list(set(counter.values()) - {1,2})
+val.sort()
+
+for i in val:
+    for key, value in counter.items():
+        if value == i:
+            result.append(len(key))
+try:
+    if max(val) < 3:
+        print(-1)
+    else:
+        print(min(result))
+except:
+    print(-1)
+    
+
