5ac3b9ab4816351021b21bf833eceefa0850716e
Hueok <developerhueok@gmail.com>
2024-01-31 19:03:36 +0900

    a

--- s4_4949_parenthesesBalance/main.cpp
+++ s4_4949_parenthesesBalance/main.cpp
@@ -3,8 +3,15 @@
 
 int main(){
     std::string sentence;
+    bool small;
+    bool big;
+    int start_idx;
+    int end_idx;
     while(true){
         getline(std::cin, sentence, '.');
-        std::cout << sentence;
+        std::cin.ignore();
+        if(sentence == ".") std::cout << "test";
+        start_idx = 0;
+        end_idx = sentence.size()-1;
     }
 }
