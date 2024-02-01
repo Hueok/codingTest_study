7029c48c512859d5b6628df5ea7bf8814b1bbe02
Hueok <developerhueok@gmail.com>
2024-02-02 04:38:14 +0900

    fail

--- s4_4949_parenthesesBalance/main.cpp
+++ s4_4949_parenthesesBalance/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(){
     std::string sentence;
-    bool small;
-    bool big;
+    std::vector<bool> small;
+    std::vector<bool> big;
+    bool status;
     int start_idx;
     int end_idx;
     while(true){
         getline(std::cin, sentence, '.');
         std::cin.ignore();
-        if(sentence == ".") std::cout << "test";
+        if(sentence == "exit") break;
         start_idx = 0;
         end_idx = sentence.size()-1;
+        while(start_idx < end_idx){
+            if(sentence[start_idx] == '('){
+                small.push_back(true);
+            } else if(sentence[start_idx] == '['){
+                big.push_back(true);
+            } else if(sentence[start_idx] == ')'){
+                if(small.empty()){
+                    status = false;
+                    break;
+                }
+            }
+        }
     }
 }
