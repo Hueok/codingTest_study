a2041e65e95d9e62d44aec36c71ff2a2e1891775
Hueok <developerhueok@gmail.com>
2024-02-04 04:39:26 +0900

    solve 4949

--- s4_4949_parenthesesBalance/main.cpp
+++ s4_4949_parenthesesBalance/main.cpp
@@ -2,30 +2,64 @@
 #include <string>
 #include <vector>
 
-int main(){
-    std::string sentence;
-    std::vector<bool> small;
-    std::vector<bool> big;
-    bool status;
-    int start_idx;
-    int end_idx;
-    while(true){
-        getline(std::cin, sentence, '.');
-        std::cin.ignore();
-        if(sentence == "exit") break;
-        start_idx = 0;
-        end_idx = sentence.size()-1;
-        while(start_idx < end_idx){
-            if(sentence[start_idx] == '('){
-                small.push_back(true);
-            } else if(sentence[start_idx] == '['){
-                big.push_back(true);
-            } else if(sentence[start_idx] == ')'){
-                if(small.empty()){
-                    status = false;
-                    break;
-                }
+class Bracket{
+public : 
+    int this_count;
+    int child_count;
+    Bracket();
+    ~Bracket();
+};
+Bracket::Bracket():this_count(1), child_count(0) {};
+Bracket::~Bracket(){
+
+}
+
+void clearAlloc(std::vector<Bracket*>& storage){
+    for(auto element : storage) delete element; 
+}
+
+bool checkVPS(const std::string& sen){
+    std::vector<Bracket*> small_storage;
+    std::vector<Bracket*> big_storage;
+    for(char c : sen){
+        if(c == '('){
+            small_storage.push_back(new Bracket());
+            if(!big_storage.empty()) big_storage.back()->child_count++;
+        } else if(c == ')'){
+            if(small_storage.empty() || small_storage.back()->child_count > 0 ){
+                clearAlloc(small_storage);
+                return false;
+            } else {
+                delete small_storage.back();
+                small_storage.pop_back();
+                if(!big_storage.empty()) big_storage.back()->child_count--;
+            }
+
+        } else if(c == '['){
+            big_storage.push_back(new Bracket());
+            if(!small_storage.empty()) small_storage.back()->child_count++; 
+        } else if(c == ']'){
+            if(big_storage.empty() || big_storage.back()->child_count > 0){
+                clearAlloc(big_storage);
+                return false;
+            } else{
+                delete big_storage.back();
+                big_storage.pop_back();
+                if(!small_storage.empty()) small_storage.back()->child_count--;
             }
         }
+    }
+    return small_storage.empty() && big_storage.empty() ? true : false;
+}
+
+int main(){
+    std::string sentence;
+    
+    while(true){
+        std::getline(std::cin, sentence,'.');
+        std::cin.ignore();
+        if(sentence.empty()) break;
+        std::string result = checkVPS(sentence) ? "yes" : "no";
+        std::cout << result << std::endl;
     }
 }
