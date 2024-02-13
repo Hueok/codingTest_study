f93328baf3de08a92a7003e245dcce79cdd4ce61
Hueok <124287568+Hueok@users.noreply.github.com>
2024-02-14 06:26:10 +0900

    ㅁㄴㅇㄹ

---
+++ s2_1012_cabbage/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+
+int main(){
+    int T;
+    std::cin >> T;
+    for(int i=0; i<T; i++){
+        int N;
+        int M;
+        std::cin >> N >> M;
+        std::vector<std::vector<int>> arr(N, std::vector<int>(M));
+        std::vector<std::vector<bool>> arr(N, std::vector<bool>(M));
+        
+
+    }
+    
+
+}
