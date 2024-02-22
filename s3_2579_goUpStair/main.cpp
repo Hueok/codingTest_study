adfbbafe3688b06e4a0c26eaac789d58ca154b6d
Hueok <developerhueok@gmail.com>
2024-02-23 08:45:29 +0900

    solve 2479

--- s3_2579_goUpStair/main.cpp
+++ s3_2579_goUpStair/main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-std::vector<int> stairs;
+std::vector<int> stairs(301);
 
 int main(){
     int N;
     std::cin >> N;
-    for(int i=0; i<N; i++){
+    for(int i=1; i<N+1; i++){
         int tmp;
         std::cin >> tmp;
-        stairs.push_back(tmp);
+        stairs[i] = tmp;
     }
-    int dp[301] = {0, };
-    dp[1] = stairs.front();
-    for(int i=2; i<N+1; i++){
-        if(dp[i-1] and dp[i-2]) dp[i] = dp[i-1] + stairs[i-1];
-
+    std::vector<std::pair<int, int>> dp(301, {0,0});
+    //<consecutive accumulation, jump accumulation>
+    dp[1] = {stairs[1], 0};
+    dp[2] = {stairs[1] + stairs[2], stairs[2]};
+    for(int i=3; i<N+1; i++){
+        dp[i] = {dp[i-1].second + stairs[i], std::max(dp[i-2].first, dp[i-2].second) + stairs[i]};
     }
+    std::cout << std::max(dp[N].first, dp[N].second) << std::endl;
 
 }
