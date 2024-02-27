1cf9daa72791e62b3c3d4f2c0a334e29240a4694
Hueok <124287568+Hueok@users.noreply.github.com>
2024-02-28 00:58:12 +0900

    update 7576

--- g5_7576_tomato/main.cpp
+++ g5_7576_tomato/main.cpp
@@ -1,31 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+
 
 int N, M;
-bool day(std::vector<std::vector<int>>& field){
-    std::vector<std::vector<bool>> visited(N, std::vector<bool>(M, false));
-    bool status = false;
-    int D[4] = {1, -1, 0, 0};
-    for(int i=0; i<N; i++){
-        for(int j=0; j<M; j++){
-            if(field[i][j] == 1 && !visited[i][j]){
-                visited[i][j] = true;
-                for(int k=0; k<4; k++){
-                    int nx = i+D[k];
-                    int ny = j+D[3-k];
-                    if(!(nx < 0 or nx >= N or ny < 0 or ny >= M or field[nx][ny]!=0 or visited[nx][ny])){
-                        visited[nx][ny] = true;
-                        field[nx][ny]++;
-                        status = true;
-                    }
-                }
-            }
-        }
-    }
-    return status;
-    //if something changed, return true else false
-}
-bool isAllRipe(const std::vector<std::vector<int>>& field){
+bool isAllRipe(const std::vector<std::vector<int> >& field){
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
             if(field[i][j] == 0) return false;
@@ -33,7 +12,7 @@ bool isAllRipe(const std::vector<std::vector<int>>& field){
     }
     return true;
 }
-void printField(const std::vector<std::vector<int>>& field){
+void printField(const std::vector<std::vector<int> >& field){
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
             std::cout << field[i][j] << " ";
@@ -46,21 +25,40 @@ int main(){
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     std::cin >> M >> N;
-    std::vector<std::vector<int>> field(N, std::vector<int>(M));
+    std::vector<std::vector<int> > field(N, std::vector<int>(M));
+    std::queue<std::pair<int, int> > q;
+    std::vector<std::vector<int> > visited(N, std::vector<int>(M, 0));
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
             std::cin >> field[i][j];
+            if(field[i][j] == 1){
+                q.push(std::make_pair(i, j));
+            }
         }
     }
+    int D[4] = {1, -1, 0, 0};
     int cnt = 0;
-    while(day(field)){
-        cnt++;
-        // std::cout << "day : " << cnt << "---------------" << std::endl;
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        // std::cout << "day : " << cnt << " ------------" << std::endl;
         // printField(field);
+        q.pop();
+        for(int i=0; i<4; i++){
+            int nx = x+D[i];
+            int ny = y+D[3-i];
+            if(nx<0 or nx>=N or ny<0 or ny>=M or visited[nx][ny] or field[nx][ny]==-1) continue;
+            visited[nx][ny] = visited[x][y]+1;
+            if(field[nx][ny] == 0){
+                field[nx][ny]++;
+                cnt = visited[nx][ny];
+            }
+            q.push(std::make_pair(nx, ny));
+        }
     }
     if(isAllRipe(field)){
         std::cout << cnt << std::endl;
-    } else{
+    } else {
         std::cout << -1 << std::endl;
     }
     
