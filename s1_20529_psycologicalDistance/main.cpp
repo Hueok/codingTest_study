04a2e676e866572abd60a6f26be19d9a3794c53c
Hueok <124287568+Hueok@users.noreply.github.com>
2024-03-10 18:12:40 +0900

    solve 20529

--- s1_20529_psycologicalDistance/main.cpp
+++ s1_20529_psycologicalDistance/main.cpp
@@ -11,6 +11,8 @@ std::string arr[16] = {
     "ESTP", "ESFP", "ENFP", "ENTP", "ESTJ", "ESFJ", "ENTJ", "ENFJ"
 };
 
+std::map<std::string, std::map<std::string, int> > relation;
+
 int getDistance(const std::string& lhs, const std::string& rhs){
     int d = 0;
     for(int i=0; i<4; i++){
@@ -38,12 +40,25 @@ void printContainer(T& container){
     std::cout <<"FUNCTION call ended.." << std::endl;
 }
 
+int solve(int n, std::vector<std::string>& types){
+    if(n>32) return 0;
+    int dist = 1e9;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            for(int k=j+1; k<n; k++){
+                dist = std::min(dist, relation[types[i]][types[j]] + relation[types[j]][types[k]] + relation[types[k]][types[i]]);
+            }
+        }
+    }
+    return dist;
+}
+
 int main(){
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL); std::cout.tie(NULL);
     int T, N;
     std::cin >> T;
-    std::map<std::string, std::map<std::string, int> > relation = setRelation();
+    relation = setRelation();
     for(int t=0; t<T; t++){
         std::cin >> N;
         std::vector<std::string> types;
@@ -52,7 +67,8 @@ int main(){
             std::cin >> tmp;
             types.push_back(tmp);
         }
-        // printContainer(types);
+        int result = solve(N, types);
+        std::cout << result << std::endl;
     }
     return 0;
 }
